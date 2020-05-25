e5809b4f96f4f9d8b01229f722badb14541739ba
sametozeren <sametozrn@gmail.com>
2020-05-25 18:52:11 +0300

    mongodb-connect file has been renamed

--- mongodb-connection.js
+++ mongodb-connection.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const ConsoleResult = require('./messages/ConsoleResult');
-const ServerResult = require('./messages/ServerResult');
-const ResponseMessage = require('./messages/Messages.json');
 
-async function main() {
+async function connect() {
     try {
         await mongoose.connect(
             process.env.MONGODB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/defangi', {
@@ -19,4 +17,4 @@ async function main() {
     }
 }
 
-main();
+connect();
