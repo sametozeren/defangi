3994d4375e93f557797ad39484aeb29e1746d857
sametozeren <sametozrn@gmail.com>
2020-05-27 21:46:17 +0300

    database config updated

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    jwt_secret_key:'çok gizli bir anahtar ürettik',
+    //db config
+    db_host: '127.0.0.1',
+    db_port: '27017',
+    db_name: 'defangi',
+
+    //jwt-config
+    jwt_secret_key: 'çok gizli bir anahtar ürettik',
 };
--- connectDb.js
+++ connectDb.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
+const config = require('./config');
 const ConsoleResult = require('./messages/ConsoleResult');
 
 const connectDb = async function connect() {
     try {
         await mongoose.connect(
-            process.env.MONGODB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/defangi', {
+            process.env.MONGODB_CONNECTION_STRING || `mongodb://${config.db_host}:${config.db_port}/${config.db_name}`, {
                 useUnifiedTopology: true,
                 useNewUrlParser: true
             }
         );
 
-        ConsoleResult.success('Connected');
+        ConsoleResult.success('Connected Database');
     } catch (error) {
-        //TODO: ServerResult()
-        ConsoleResult.error('Not Connected: ' + error);
+        ConsoleResult.error('Not Connected Database:' + error);
     }
 }
 
